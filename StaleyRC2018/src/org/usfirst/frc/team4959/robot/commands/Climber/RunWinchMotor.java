8fc5a8debcd94c875a52ae66fe133aefebc03a78
Staley Robotics <Staley Robotics@DESKTOP-DQ1A43E>
2018-02-05 14:42:57 -0600

    Switched to return false

--- StaleyRC2018/src/org/usfirst/frc/team4959/robot/commands/Climber/RunWinchMotor.java
+++ StaleyRC2018/src/org/usfirst/frc/team4959/robot/commands/Climber/RunWinchMotor.java
@@ -26,15 +26,17 @@ public class RunWinchMotor extends Command {
 
     // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
-        return true;
+        return false;
     }
 
     // Called once after isFinished returns true
     protected void end() {
+    	Robot.climber.runWinch(0);
     }
 
     // Called when another command which requires one or more of the same
     // subsystems is scheduled to run
     protected void interrupted() {
+    	end();
     }
 }
