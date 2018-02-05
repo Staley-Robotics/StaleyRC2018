c949abbab1974ff55b544402b4bda0b0dbd503b9
Staley Robotics <Staley Robotics@DESKTOP-DQ1A43E>
2018-02-05 14:46:50 -0600

    Added constants for the different scale elevations

--- StaleyRC2018/src/org/usfirst/frc/team4959/robot/util/Constants.java
+++ StaleyRC2018/src/org/usfirst/frc/team4959/robot/util/Constants.java
@@ -8,7 +8,9 @@ public class Constants {
 	// Set elevations for the elevator
 	public static final double ELEVATOR_BOTTOM_ELEVATION = 0;
 	public static final double ELEVATOR_SWITCH_ELEVATION = 20000;
-	public static final double ELEVATOR_SCALE_ELEVATION = 100000;
+	public static final double ELEVATOR_LOW_SCALE_ELEVATION = 100000;
+	public static final double ELEVATOR_MID_SCALE_ELEVATION = 120000; 
+	public static final double ELEVATOR_HIGH_SCALE_ELEVATION = 140000;
 	
 	// Soft limits for how far the elevator can go
 	public static final int FWD_SOFT_LIMIT = 20000; // Adjust this later on
