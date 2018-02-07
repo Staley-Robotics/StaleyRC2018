c3fe02d622351e8f0421d4c713c349f246b75e94
Staley Robotics <Staley Robotics@DESKTOP-DQ1A43E>
2018-02-06 20:03:16 -0600

    Added constants

--- StaleyRC2018/src/org/usfirst/frc/team4959/robot/util/Constants.java
+++ StaleyRC2018/src/org/usfirst/frc/team4959/robot/util/Constants.java
@@ -2,8 +2,8 @@ package org.usfirst.frc.team4959.robot.util;
 
 public class Constants {
 	// Intake speeds for taking in and sending out
-	public static final double INTAKE_IN_SPEED = 0.4;
-	public static final double INTAKE_OUT_SPEED = -0.7;
+	public static final double INTAKE_IN_SPEED = -0.4;
+	public static final double INTAKE_OUT_SPEED = 0.7;
 	
 	// Set elevations for the elevator
 	public static final double ELEVATOR_BOTTOM_ELEVATION = 0;
@@ -16,7 +16,9 @@ public class Constants {
 	public static final int FWD_SOFT_LIMIT = 20000; // Adjust this later on
 	public static final int REV_SOFT_LIMIT = 0;
 	
-	// Pulses per revolution of encoders
-	public static final double ENCODER_DISTANCE_PER_PULSE_POSITIVE = 185;
-	public static final double ENCODER_DISTANCE_PER_PULSE_NEGATIVE = 175;
+	// ***** Encoder Constants *****
+	public static final double ENCODER_DISTANCE_PER_PULSE = 210; // Pulses per revolution of encoders
+	public static final double TICKS_PER_REVOLUTION = 20;
+	public static final double WHEEL_DIAMETER = 6;
+	public static final double GEAR_RATIO = 1/15;
 }
