e6f17769ff451bc2235b1eeb8530bd6b89be6050
Staley Robotics 4959 <staleyrobotics@gmail.com>
2018-02-09 13:30:15 -0600

    Adjusted values for intake speed and elevator positions.

--- StaleyRC2018/src/org/usfirst/frc/team4959/robot/util/Constants.java
+++ StaleyRC2018/src/org/usfirst/frc/team4959/robot/util/Constants.java
@@ -3,14 +3,14 @@ package org.usfirst.frc.team4959.robot.util;
 public class Constants {
 	// Intake speeds for taking in and sending out
 	public static final double INTAKE_IN_SPEED = -0.4;
-	public static final double INTAKE_OUT_SPEED = 0.7;
+	public static final double INTAKE_OUT_SPEED = 0.2;
 	
 	// Set elevations for the elevator
-	public static final double ELEVATOR_BOTTOM_ELEVATION = 0;
-	public static final double ELEVATOR_SWITCH_ELEVATION = 20000;
-	public static final double ELEVATOR_LOW_SCALE_ELEVATION = 100000;
-	public static final double ELEVATOR_MID_SCALE_ELEVATION = 120000; 
-	public static final double ELEVATOR_HIGH_SCALE_ELEVATION = 140000;
+	public static final double ELEVATOR_BOTTOM_ELEVATION = -1000;
+	public static final double ELEVATOR_SWITCH_ELEVATION = 220000;
+	public static final double ELEVATOR_LOW_SCALE_ELEVATION = 720000;
+	public static final double ELEVATOR_MID_SCALE_ELEVATION = 755000; 
+	public static final double ELEVATOR_HIGH_SCALE_ELEVATION = 765000;
 	
 	// Soft limits for how far the elevator can go
 	public static final int FWD_SOFT_LIMIT = 20000; // Adjust this later on
