386c3c3ba3472d7142136ee0e5a8efedfbfee7e4
Staley Robotics 4959 <staleyrobotics@gmail.com>
2018-02-12 18:24:49 -0600

    Adjusted numbers

--- StaleyRC2018/src/org/usfirst/frc/team4959/robot/util/Constants.java
+++ StaleyRC2018/src/org/usfirst/frc/team4959/robot/util/Constants.java
@@ -11,20 +11,22 @@ public class Constants {
 	
 	// Set elevations for the elevator
 	public static final double ELEVATOR_BOTTOM_ELEVATION = -1000;
-	public static final double ELEVATOR_SWITCH_ELEVATION = 220000;
-	public static final double ELEVATOR_LOW_SCALE_ELEVATION = 720000;
-	public static final double ELEVATOR_MID_SCALE_ELEVATION = 755000; 
-	public static final double ELEVATOR_HIGH_SCALE_ELEVATION = 765000;
+	public static final double ELEVATOR_SWITCH_ELEVATION = 235000;
+	public static final double ELEVATOR_LOW_SCALE_ELEVATION = 555000;
+	public static final double ELEVATOR_MID_SCALE_ELEVATION = 755000;	//not in use 
+	public static final double ELEVATOR_HIGH_SCALE_ELEVATION = 672000;
 	
 	// Soft limits for how far the elevator can go (Not in use)
 	public static final int FWD_SOFT_LIMIT = 20000; // Adjust this later on
-	public static final int REV_SOFT_LIMIT = 0;
+	public static final int REV_SOFT_LIMIT = -5000;
 	
 	// ***** Encoder Constants *****
-	public static final double ENCODER_DISTANCE_PER_PULSE = 210; // Pulses per revolution of encoders
+//	public static final double ENCODER_DISTANCE_PER_PULSE = 0.07575; // Pulses per revolution of encoders
+	public static final double ENCODER_DISTANCE_PER_PULSE = 0.056; // Pulses per revolution of encoders
+	public static final double PULSE_PER_INCH = 13.2;
 	public static final double TICKS_PER_REVOLUTION = 20;
 	
 	// Physical constants for the drive train
 	public static final double WHEEL_DIAMETER = 6;
-	public static final double GEAR_RATIO = 1/15;
+	public static final double GEAR_RATIO = 0.1;
 }
