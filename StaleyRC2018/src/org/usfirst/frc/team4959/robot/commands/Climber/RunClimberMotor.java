c22d60bd17ff7d2d3769568b4c40d409572712b1
Staley Robotics 4959 <staleyrobotics@gmail.com>
2018-02-12 18:23:01 -0600

    Moved to right joystick and added rumble

--- StaleyRC2018/src/org/usfirst/frc/team4959/robot/commands/Climber/RunClimberMotor.java
+++ StaleyRC2018/src/org/usfirst/frc/team4959/robot/commands/Climber/RunClimberMotor.java
@@ -2,41 +2,49 @@ package org.usfirst.frc.team4959.robot.commands.Climber;
 
 import org.usfirst.frc.team4959.robot.Robot;
 
+import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.command.Command;
 
 /**
  * Runs the winch to pull us up to the rung
  */
 public class RunClimberMotor extends Command {
-	
+
 	private double power;
 
-    public RunClimberMotor(double power) {
-    	this.power = power;
-    }
+	public RunClimberMotor(double power) {
+		requires(Robot.climber);
+		this.power = power;
+	}
 
-    // Called just before this Command runs the first time
-    protected void initialize() {
-    }
+	// Called just before this Command runs the first time
+	protected void initialize() {
+	}
 
-    // Called repeatedly when this Command is scheduled to run
-    protected void execute() {
-    	Robot.climber.runClimber(power);
-    }
+	// Called repeatedly when this Command is scheduled to run
+	protected void execute() {
+		if (Robot.m_oi.getRightStickYCont2() >= 0.10 || Robot.m_oi.getRightStickYCont2() <= -0.1) {
+			Robot.climber.runClimber(power);
+			Robot.m_oi.xboxControllertwo.setRumble(GenericHID.RumbleType.kRightRumble, Math.abs(Robot.m_oi.getRightStickYCont2()));
+		}
+		else {
+			Robot.m_oi.xboxControllertwo.setRumble(GenericHID.RumbleType.kRightRumble, 0);
+		}
+	}
 
-    // Make this return true when this Command no longer needs to run execute()
-    protected boolean isFinished() {
-        return false;
-    }
+	// Make this return true when this Command no longer needs to run execute()
+	protected boolean isFinished() {
+		return false;
+	}
 
-    // Called once after isFinished returns true
-    protected void end() {
-    	Robot.climber.runClimber(0);
-    }
+	// Called once after isFinished returns true
+	protected void end() {
+		Robot.climber.runClimber(0);
+	}
 
-    // Called when another command which requires one or more of the same
-    // subsystems is scheduled to run
-    protected void interrupted() {
-    	end();
-    }
+	// Called when another command which requires one or more of the same
+	// subsystems is scheduled to run
+	protected void interrupted() {
+		end();
+	}
 }
