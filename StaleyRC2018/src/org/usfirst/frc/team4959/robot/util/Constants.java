1966606598b38c4b2158ac8433a5aec4ee99b328
Staley Robotics 4959 <staleyrobotics@gmail.com>
2018-02-16 21:34:40 -0600

    Adjusted constants and added elevator current limits

--- StaleyRC2018/src/org/usfirst/frc/team4959/robot/util/Constants.java
+++ StaleyRC2018/src/org/usfirst/frc/team4959/robot/util/Constants.java
@@ -6,27 +6,31 @@ package org.usfirst.frc.team4959.robot.util;
 
 public class Constants {
 	// Intake speeds for taking in and sending out
-	public static final double INTAKE_IN_SPEED = -0.4;
-	public static final double INTAKE_OUT_SPEED = 0.2;
+	public static final double INTAKE_IN_SPEED = 0.4;
+	public static final double INTAKE_OUT_SPEED = -0.4;
 	
 	// Set elevations for the elevator
-	public static final double ELEVATOR_BOTTOM_ELEVATION = -1000;
+	public static final double ELEVATOR_BOTTOM_ELEVATION = 3000;
 	public static final double ELEVATOR_SWITCH_ELEVATION = 235000;
-	public static final double ELEVATOR_LOW_SCALE_ELEVATION = 555000;
-	public static final double ELEVATOR_MID_SCALE_ELEVATION = 755000;	//not in use 
-	public static final double ELEVATOR_HIGH_SCALE_ELEVATION = 672000;
+	public static final double ELEVATOR_LOW_SCALE_ELEVATION = 565000;
+	public static final double ELEVATOR_MID_SCALE_ELEVATION = 600000;	//not in use 
+	public static final double ELEVATOR_HIGH_SCALE_ELEVATION = 675000;
 	
-	// Soft limits for how far the elevator can go (Not in use)
-	public static final int FWD_SOFT_LIMIT = 20000; // Adjust this later on
-	public static final int REV_SOFT_LIMIT = -8000;
+	// Soft limits for how far the elevator can go 
+	public static final int FWD_SOFT_LIMIT = 680000;
+	public static final int REV_SOFT_LIMIT = -4000;
 	
 	// ***** Encoder Constants *****
-//	public static final double ENCODER_DISTANCE_PER_PULSE = 0.07575; // Pulses per revolution of encoders
-	public static final double ENCODER_DISTANCE_PER_PULSE = 0.056; // Pulses per revolution of encoders
+//	public static final double ENCODER_DISTANCE_PER_PULSE = 0.056; // Pulses per revolution of encoders low gear
+	public static final double ENCODER_DISTANCE_PER_PULSE = 0.201; // High gear
 	public static final double PULSE_PER_INCH = 13.2;
 	public static final double TICKS_PER_REVOLUTION = 20;
 	
 	// Physical constants for the drive train
 	public static final double WHEEL_DIAMETER = 6;
 	public static final double GEAR_RATIO = 0.1;
+	
+	// Current Limits for Elevator Motors
+	public static final double HIGHEST_ELEVATOR_CURRENT = 130;
+	public static final double LOWEST_ELEVATOR_CURRENT = 120;
 }
