5f0b78be53557d9a0ed518c5ce9dca7dd0dd7948
Staley Robotics 4959 <staleyrobotics@gmail.com>
2018-02-16 21:40:40 -0600

    Added object from LiveVariableStory to be sure it maintains correct spot

--- StaleyRC2018/src/org/usfirst/frc/team4959/robot/commands/Elevator/ZeroElevator.java
+++ StaleyRC2018/src/org/usfirst/frc/team4959/robot/commands/Elevator/ZeroElevator.java
@@ -1,6 +1,7 @@
 package org.usfirst.frc.team4959.robot.commands.Elevator;
 
 import org.usfirst.frc.team4959.robot.Robot;
+import org.usfirst.frc.team4959.robot.util.LiveVariableStory;
 
 import edu.wpi.first.wpilibj.command.Command;
 
@@ -20,6 +21,7 @@ public class ZeroElevator extends Command {
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
     	Robot.elevator.zeroPosition();
+    	LiveVariableStory.pos = 0;
     }
 
     // Make this return true when this Command no longer needs to run execute()
