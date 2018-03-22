cef3070c948caf372ccb5dddb5b4e2789fdeed15
Staley Robotics <Staley Robotics@DESKTOP-DQ1A43E>
2018-03-21 22:12:42 -0500

    Added limit switch

--- StaleyRC2018/src/org/usfirst/frc/team4959/robot/commands/Elevator/MoveElevator.java
+++ StaleyRC2018/src/org/usfirst/frc/team4959/robot/commands/Elevator/MoveElevator.java
@@ -24,40 +24,50 @@ public class MoveElevator extends Command {
 
 	// Called just before this Command runs the first time
 	protected void initialize() {
+
 	}
 
 	// Called repeatedly when this Command is scheduled to run
 	protected void execute() {
 		if (States.elevatorControlState == States.ElevatorControlStates.joystickControl) {
+			// Going down
 			if (Robot.m_oi.getLeftStickYCont2() > Constants.JOYSTICK_Y_AXIS_DEADZONE) {
-				States.elevatorPosState = States.ElevatorPosStates.userControl; // Verifies that the robot is being
-																				// controlled by the user
-				elevator.moveElevator(Robot.m_oi.getLeftStickYCont2());
-				System.out.println(TAG + " Joystickvalue Pos: " + Robot.m_oi.getLeftStickYCont2());
-				LiveVariableStory.pos = elevator.getPosition();
-
-				System.out.println(TAG + "Going up");
-			} else if (Robot.m_oi.getLeftStickYCont2() < -Constants.JOYSTICK_Y_AXIS_DEADZONE) {
+				if (!elevator.getLimitSwitch()) {
+					States.elevatorPosState = States.ElevatorPosStates.userControl; // Verifies that the robot is being
+																					// controlled by the user
+					elevator.moveElevator(Robot.m_oi.getLeftStickYCont2());
+					System.out.println(TAG + " Joystickvalue Pos: " + Robot.m_oi.getLeftStickYCont2());
+					LiveVariableStory.pos = elevator.getPosition();
+				}
+			}
+			// Going up
+			else if (Robot.m_oi.getLeftStickYCont2() < -Constants.JOYSTICK_Y_AXIS_DEADZONE) {
 				States.elevatorPosState = States.ElevatorPosStates.userControl; // Verifies that the robot is being
 																				// controlled by the user
 				elevator.moveElevator(Robot.m_oi.getLeftStickYCont2());
 				System.out.println(TAG + " Joystickvalue Neg: " + Robot.m_oi.getLeftStickYCont2());
 				LiveVariableStory.pos = elevator.getPosition();
-
-				System.out.println(TAG + "Going down");
-			} else {
+			}
+			// Holding position
+			else {
 				States.elevatorPosState = States.ElevatorPosStates.positionHeld;
 				// elevator.stopElevator();
-
-				if (States.elevatorSoftLimitState == States.ElevatorSoftLimitStates.enabled) {
-					if (LiveVariableStory.pos < Constants.REV_SOFT_LIMIT) {
-						LiveVariableStory.pos = 0;
-					}
-				}
+				
+				// Dont touch unless you are willing to fight the demons that this block releases
+				// if (States.elevatorSoftLimitState == States.ElevatorSoftLimitStates.enabled)
+				// {
+				// if (LiveVariableStory.pos < Constants.REV_SOFT_LIMIT) {
+				// LiveVariableStory.pos = 0;
+				// }
+				// }
 				elevator.setPosition(LiveVariableStory.pos); // To help maintain the elevator's position
 
 				// System.out.println(TAG + "Staying put at " + LiveVariableStory.pos);
 			}
+			
+			// While the limit switch is being hit, zero the encoder
+			if (elevator.getLimitSwitch())
+				elevator.zeroPosition();
 		}
 	}
 
